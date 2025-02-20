19a2bf32e7cdcc6ecea963637d2f7940065ebeff
slemi23 <slemi@unomaha.edu>
2025-02-19 23:05:52 -0600

    Changing the greetings

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,5 @@
 // Simple C++ program to display "Hello World" 
+// Author: Sarah Lemi <slemi@unomaha.edu>
 // Header file for input output functions 
 #include<iostream>  
   
@@ -9,7 +10,7 @@ using namespace std;
 int main() 
 { 
     // prints hello world 
-    cout<<"Hello World"; 
+    cout<<"Testing git merge1"; 
       
     return 0; 
 } 
